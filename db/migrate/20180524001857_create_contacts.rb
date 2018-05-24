1900cdac815daa2dba82ccb91393a44e5f3c1a99
Michael Stone <codemjstone06@gmail.com>
2018-05-24 00:22:38 +0000

    model set up

---
+++ db/migrate/20180524001857_create_contacts.rb
@@ -0,0 +1,11 @@
+class CreateContacts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :contacts do |t|
+      t.string :name
+      t.integer :phone
+      t.string :email
+      t.text :enquiry
+      t.timestamps
+    end
+  end
+end
